068c76fd0a9f7efc90019b2fd4c5bce314476db9
Edric <edhernal@yahoo.com>
2023-08-29 18:26:48 -0700

    added math functions

---
+++ script.js
@@ -0,0 +1,19 @@
+//addition function
+const add(a , b) {
+    return a + b;
+}
+
+//subtraction function
+const subtract(a , b) {
+    return a - b;
+}
+//multiply function
+const multiply(a ,b) {
+    return a * b;
+}
+
+const divide(a ,b) {
+    if (b === 0) {
+    return "cannont divide by 0"
+    } return a / b;
+}
