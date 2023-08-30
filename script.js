7bade2ab158fb1545bedb5ed0bdd93a97840dcdc
Edric <edhernal@yahoo.com>
2023-08-29 18:48:42 -0700

    added operation function

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 //addition function
-const add(a , b) {
+function add(a , b) {
     return a + b;
 }
 
 //subtraction function
-const subtract(a , b) {
+function subtract(a , b) {
     return a - b;
 }
 //multiply function
-const multiply(a ,b) {
+function multiply(a ,b) {
     return a * b;
 }
 
-const divide(a ,b) {
+function divide(a ,b) {
     if (b === 0) {
     return "cannont divide by 0"
     } return a / b;
 }
+
+const firstNumber = 0;
+const secondNumber = 0;
+const operator = null;
+
+//operate function
+function operate(num1, operator, num2) {
+   switch(operator) {
+    case '+':
+        return add(num1, num2);
+    case '-':
+        return subtract(num1, num2);
+    case '*':
+        return multiply(num1, num2);
+    case '/':
+        return divide(num1, num2);
+    }
+}
+
+console.log(operate(10 , "-" , 3));
